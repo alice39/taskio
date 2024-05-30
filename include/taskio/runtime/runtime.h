de6703243af935a82961db3130119a6b2bd6fb22
A.L.I.C.E <20363359+alice39@users.noreply.github.com>
2024-05-30 19:22:43 -0400

    feat(runtime): add taskio_task_spawn macros at runtime.h

--- include/taskio/runtime/runtime.h
+++ include/taskio/runtime/runtime.h
@@ -1,10 +1,17 @@
-#ifndef TASKIO_RUNTIME_GUARD_HEADEr
+#ifndef TASKIO_RUNTIME_GUARD_HEADER
 #define TASKIO_RUNTIME_GUARD_HEADER
 
 #include <stdbool.h>
 
 #include "../task/future.h"
 #include "../task/task.h"
+
+#define taskio_task_spawn(future) taskio_runtime_spawn(taskio_task_new(future))
+#define taskio_task_spawn_fn(fn, ...)                                          \
+    {                                                                          \
+        struct fn##_future __future_obj_to_spawn = fn(__VA_ARGS__);            \
+        taskio_runtime_spawn(taskio_task_new(__future_obj_to_spawn));          \
+    }
 
 struct taskio_join_handle {
     void (*await)(struct taskio_task*, void*);
--- tests/simple_runtime_test.c
+++ tests/simple_runtime_test.c
@@ -8,11 +8,16 @@
 
 async_fn(int, foo, {
     int a = 123;
+
     await_fn(taskio_sleep, 1, 0);
     printf("foo: slept 1 second. %d\n", a);
+
     a++;
+
     await_fn(taskio_sleep, 3, 0);
     printf("foo: slept 3 seconds. %d\n", a);
+
+    async_return(a);
 });
 
 async_fn(void, bar, {
@@ -21,8 +26,6 @@ async_fn(void, bar, {
 });
 
 taskio_main({
-    struct foo_future f1 = foo();
-    struct bar_future f2 = bar();
-
-    await_fn(taskio_join, &f1, &f2);
+    taskio_task_spawn_fn(foo);
+    taskio_task_spawn_fn(bar);
 });
